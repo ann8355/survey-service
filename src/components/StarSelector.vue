ab390148d0bc3b692f85940b62a46b05e6362ddf
ann8355 <s8900771@gmail.com>
2020-01-16 15:57:24 +0800

    add starselector

---
+++ src/components/StarSelector.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="star">
+    <input class="starCheck" type="checkbox">
+    <label id="star" for="star" class="yif icon-star-empty"></label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StarSelector',
+  data() {
+    return {
+    };
+  },
+  props: {
+  },
+  watch: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '~@/assets/css/base';
+
+#star:hover::before, .starCheck:checked ~ #star::before{
+  content:"\e90a";
+  color: #F5A623;
+}
+.starCheck{
+  display: none;
+  position: absolute !important;
+}
+#star {
+  cursor: pointer;
+}
+</style>
